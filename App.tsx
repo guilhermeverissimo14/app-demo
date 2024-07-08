e23a81666dc02bbe6fb8270a67dbe3a47e42812f
guilhermeverissimo14 <guilhermeverissimo14@gmail.com>
2024-07-08 15:26:46 -0300

    feat:adding a theme

--- App.tsx
+++ App.tsx
@@ -1,7 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import AppRoutes from './src/routes/app.routes';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter';
+
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null; //criar um loading
+  }
+
   return (
     <>
       <AppRoutes />
---
+++ src/theme/theme.ts
@@ -0,0 +1,36 @@
+export const theme = {
+    colors: {
+        white: {
+            300: "#FFFFFF",
+            400: "#FBFBFB",
+            500: "#F2F2F2",
+        },
+        primary: '#6759FF',
+        secondary: "#CABDFF",
+        black: '#1A1D1F',
+        green: "#B5E4CA",
+        gray: {
+            400: "#6F767E",
+            500: "#9B9E9F"
+        },
+        orange: {
+            400: "#FFBC99",
+            500: "#FC944D"
+        }
+
+    },
+    fonts: {
+        Inter_700Bold: 'Inter_700Bold',
+        Inter_600: 'Inter_600SemiBold',
+        Inter_500: 'Inter_500Medium',
+        Inter_400: 'Inter_400Regular',
+    },
+    size: {
+        xs: 12,
+        sm: 14,
+        md: 16,
+        mdl: 18,
+        lg: 20,
+        xl: 24,
+    },
+};
